76cf615365991e6c11ee78e54b9cd4baed5faa9a
Seunope <seunope2004@yahoo.co.uk>
2018-08-14 12:25:18 +0100

    more query options

--- app/Http/Controllers/Api/QuestionController.php
+++ app/Http/Controllers/Api/QuestionController.php
@@ -23,16 +23,45 @@ class QuestionController extends Controller
             try{
                 $question = new QLoader;
                 $question->setTable($subjectTable);
-                $data = $question->inRandomOrder()->first();
+
+                if(isset($input['year']) && isset($input['type'])  ){
+                    $data = $question->where(['examtype' =>$input['type'],'examyear' =>$input['year']])->inRandomOrder()->first();
+                }else if(isset($input['year'])  ){
+                    $data = $question->where(['examyear' =>$input['year'] ])->inRandomOrder()->first();
+                }else if(isset($input['type'])  ){
+                    $data = $question->where(['examtype' =>$input['type'] ])->inRandomOrder()->first();
+                }else{
+                    $data = $question->inRandomOrder()->first();
+                }
+
+                if(empty($data)){
+                    $data['message'] = "No record found or Bad request.";
+                    $data['status'] = 400;
+                    return response()->json($data, 400, [], JSON_PRETTY_PRINT);
+                }
 
 //                $question->requestCount=1;
 //                $question->update(['requestCount' =>1]);
+                $res['subject'] = $subjectTable;
+                $res['status'] = 200;
+                $res['data']  = $data;
+                return response()->json($res, 200, [], JSON_PRETTY_PRINT);
 
-                return response()->json($data, 200, [], JSON_PRETTY_PRINT);
+
             }catch (\Exception $e){
-                $subject = (object) ['english','mathematics','commerce', 'account','biology','physics','chemistry'];
+                $subject = (object) ['english','mathematics','commerce', 'account','biology','physics','chemistry','englishlit','government','crk','geography','economics','irk','civiledu','insurance','currentaffairs','history'];
+                $type = (object) ['waec', 'jamb', 'neco','post-utme'];
+                $querySample = (object) ['q?subject=account',
+                                        'q?subject=account&year=2010',
+                                        'q?subject=account&type=waec',
+                                        'q?subject=account&year=2014&type=jamb'];
+
                 $data ['error'] = "Something strange just happened";
-                $data ['hint'] = ['message'=>'This is the list of supported subjects.', 'subject'=> $subject];
+                $data['status'] = 406;
+                $data ['hint'] = ['message-1'=>'This is the list of supported subjects.', 'Subjects'=> $subject,
+                                  'message-2'=>'Supported exam types.', 'Exams'=> $type,
+                                  'message-3'=>'Query samples.', 'Queries'=> $querySample,];
+
 
                 return response()->json($data, 406, [], JSON_PRETTY_PRINT);
             }
