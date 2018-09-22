53833bb669d7d16d474d6fd11d0b5cb6eda6f7a3
Seunope <seunope2004@yahoo.co.uk>
2018-09-22 11:54:31 +0100

    implemented subjects metrics

--- app/Http/Controllers/Api/MetricsController.php
+++ app/Http/Controllers/Api/MetricsController.php
@@ -11,7 +11,25 @@ class MetricsController extends Controller
 {
     public function subjectsApiCallCounts(){
 
+        $subjectTable = subjectArray();
+        $totalSubject = count($subjectTable);
+        $question = new QLoader;
+        $data = [];
+        for ($i=0; $i < $totalSubject; $i++){
 
+            try{
+                $question->setTable($subjectTable[$i]);
+                $subject = $question->get()->sum('requestCount');
+                $data[] = ucfirst($subjectTable[$i])." =>".$subject;
+            }catch (\Exception $e){
+                $data[] = ucfirst($subjectTable[$i])." =>Not found";
+            }
+        }
+        $res['message'] = "Metrics on API subject calls";
+        $res['status'] = 200;
+        $res['data'] = (object) $data;
+
+        return response()->json($res, 200, [], JSON_PRETTY_PRINT);
 
     }
 }
